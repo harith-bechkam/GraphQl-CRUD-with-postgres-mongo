dc61f73d40c5c188abc087a738a5ee376525b66a
harith <harith.kv47@gmail.com>
2023-06-16 14:26:30 +0530

    changes

--- postgres/dbconfig.js
+++ postgres/dbconfig.js
@@ -1,10 +1,10 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-
+let {development} = require('./config/config.json')
 const dressdb = new Sequelize(
-  process.env.DB_name,
-  process.env.postgres_username,
-  process.env.postgres_pass,
+  development.database,
+  development.username,
+  development.password,
   {
     dialect: "postgres",
     host: "localhost",
